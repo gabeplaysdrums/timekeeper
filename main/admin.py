f60edc554bf14547e9578578059b0c549769cbfd
Gabriel Y <newsboytko@gmail.com>
2012-11-20 06:14:06 -0800

    customize the admin

--- main/admin.py
+++ main/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from main.models import *
 
-admin.site.register(Timekeeper)
+
+class TimekeeperAdmin(admin.ModelAdmin):
+  list_display = ('tempo', 'timesig_display', 'duration', 'feel', 'measures_per_phrase', 'request_count')
+  list_display_links = ('request_count',)
+
+  def timesig_display(self, obj):
+    return '%d/%d' % (obj.timesig_numer, obj.timesig_denom)
+  timesig_display.short_description = 'Time Signature'
+
+admin.site.register(Timekeeper, TimekeeperAdmin)
